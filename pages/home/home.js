49d426129662a1bb859514224dc0b0a1732fbf38
weimingxuan <793270445@qq.com>
2017-06-30 21:03:12 +0800

    no message

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,11 +8,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
   },
+
   onLoad: function(){
     this._loadData();
   },
+
   _loadData: function(){
     home.getBannerData((res)=>{
       this.setData({
@@ -31,6 +32,21 @@ Page({
        productsArr: res
       });
     });
+  },
+
+  onProductsItemTap: function(event){
+    var id = home.getDataSet(event,'id');
+    wx.navigateTo({
+      url: '../product/product?id=' + id,
+    })
+  },
+
+  onThemesItemTap: function(event) {
+    var id = home.getDataSet(event, 'id');
+    var name = home.getDataSet(event, 'name');
+    wx.navigateTo({
+      url: '../theme/theme?id=' + id + '&name=' + name
+    })
   }
 
 })
